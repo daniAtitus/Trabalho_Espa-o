04d0d7f0b33cd6c625ad7a05b412fdcbc1a5bf92
Nopri010 <108908521+Nopri010@users.noreply.github.com>
2024-06-17 19:55:14 -0300

    marcação
    Marcação das estrelas e troca de icone
    3, 4 e 5

--- main.py
+++ main.py
@@ -5,19 +5,29 @@ from tkinter import simpledialog, messagebox
 pygame.init()
 altura = 563
 largura = 1000
+icone  = pygame.image.load("assets/nave.jpg")
+pygame.display.set_icon(icone)
 fundo = pygame.image.load('assets/bg.jpg')
 pygame.display.set_caption("Estrelas")
 tela = pygame.display.set_mode((largura,altura))
-
+estrelas = []
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
             pygame.quit()
-
-    tela.blit(fundo,(0,0))
+        elif evento.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                item = simpledialog.askstring("Space", "Nome da Estrela: ")
+                print(item)
+                if item is None or item == "":
+                    item = f"desconhecido{pos}"
+                estrelas.append((item, pos))
+    tela.blit(fundo, (0, 0))
+    for estrela in estrelas:
+        nome, pos = estrela
+        pygame.draw.circle(tela, (255, 255, 255), pos, 5)
+        fonte = pygame.font.Font(None, 36)
+        texto = fonte.render(nome, True, (255, 255, 255))
+        tela.blit(texto, (pos[0] + 10, pos[1] - 10))
     pygame.display.update()
-
-
 pygame.quit()
-
-
